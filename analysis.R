2c3763db02cfb480bf126ec7acb54b7971df6c1b
Satinath84 <r14853002@ntu.edu.tw>
2026-05-28 15:39:04 +0800

    analysis.R

--- analysis.R
+++ analysis.R
@@ -2,7 +2,7 @@
 # No external packages required
 
 # Import dataset
-Suman <- read.csv(file.choose(), header = TRUE)
+Suman <- read.csv("dengue_assignment.csv", header = TRUE)
 
 # Calculate total dengue cases in 2023
 total_2023 <- sum(Suman$case_number[Suman$year == 2023])
