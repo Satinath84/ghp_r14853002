de9fe89ed912c0c5b9bbab2327e4cb6d92baa1c7
Satinath84 <r14853002@ntu.edu.tw>
2026-06-01 14:02:57 +0800

    analysis.R

--- analysis.R
+++ analysis.R
@@ -7,4 +7,4 @@ library(dplyr)
 dengue <- read.csv("dengue_assignment.csv", header = TRUE)
 
 # Calculate total dengue cases in 2023
-total_2023 <- sum(Suman$case_number[Suman$year == 2023])
+total_2023 <- sum(dengue$case_number[dengue$year == 2023])
