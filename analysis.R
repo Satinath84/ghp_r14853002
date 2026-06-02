2635464744bc15c6244c14608cdb148c7220f815
Satinath84 <r14853002@ntu.edu.tw>
2026-06-02 18:19:32 +0800

    analysis.R

--- analysis.R
+++ analysis.R
@@ -1,12 +1,14 @@
-dengue<- read.csv("dengue_assignment.csv",header = TRUE)
 library(readr)
 library(dplyr)
 
 # Import dataset
-dengue <- read.csv("dengue_assignment.csv", header = TRUE)
+dengue <- read_csv("dengue_assignment.csv")
 
 # Calculate total dengue cases in 2023
-total_2023 <- sum(dengue$case_number[dengue$year == 2023])
+total_2023 <- dengue %>%
+  filter(year == 2023) %>%
+  summarise(total_cases = sum(case_number)) %>%
+  pull(total_cases)
 
 # Display result
 total_2023
